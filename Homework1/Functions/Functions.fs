d3c2a8f072be4738aed243106eafbf2b52687300
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-02-20 23:16:38 +0300

    feat: Implement the function for calculating the factorial
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ Homework1/Functions/Functions.fs
@@ -0,0 +1,14 @@
+﻿namespace Homework1
+
+module Functions =
+    let rec factorial n =
+        let rec fact acc i =
+            if i <= 1 then
+                Some acc
+            else
+                fact (acc * i) (i - 1)
+        
+        if n < 0 then
+            None
+        else
+            fact 1 n
