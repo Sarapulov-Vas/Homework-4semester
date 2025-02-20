e0e8b5917cc9bbe872c43eb5aa5d4a72a09d02f2
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-02-20 23:40:14 +0300

    feat: add tests
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ Homework1/Functions.Tests/FunctionsTests.fs
@@ -0,0 +1,11 @@
+﻿module Functions.Tests
+
+open NUnit.Framework
+open Homework1
+open FsUnit
+
+[<Test>]
+let ``Factorial Tests`` () =
+    Functions.factorial 0 |> should equal (Some 1)
+    Functions.factorial 5 |> should equal (Some 120)
+    Functions.factorial -2 |> should equal None
