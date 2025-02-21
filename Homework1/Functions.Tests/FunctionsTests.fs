726feea04f841013921c9042d6c90478712a85da
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-02-21 14:52:11 +0300

    feat: implement the function of searching for a number in the list and tests
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ Homework1/Functions.Tests/FunctionsTests.fs
@@ -0,0 +1,14 @@
+﻿module Functions.Tests
+
+open NUnit.Framework
+open Homework1
+open FsUnit
+open FsCheck
+
+let revTest (list:list<int>, n:int) =
+    Functions.numberSearch list n = List.tryFindIndex (fun x -> x = n) list
+
+/// Test of the function of searching for an item in the list.
+[<Test>]
+let ``List reverse Tests`` () =
+    Check.QuickThrowOnFailure revTest
---
+++ Homework1/Functions/Functions.fs
@@ -0,0 +1,15 @@
+﻿namespace Homework1
+
+/// Module implementing functions for homework.
+module Functions =
+    /// 
+    let rec numberSearch list n =
+        let rec search list n acc =
+            if List.isEmpty list then
+                None
+            else if List.head list = n then
+                Some acc
+            else
+                search (List.tail list) n (acc + 1) 
+                
+        search list n 0
