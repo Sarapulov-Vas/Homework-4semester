7fa0b766de6697ed278a765bdc07f142b08b600f
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-02-21 09:37:55 +0300

    feat: implement list reverse function and tests
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ Homework1/Functions.Tests/FunctionsTests.fs
@@ -0,0 +1,13 @@
+﻿module Functions.Tests
+
+open NUnit.Framework
+open Homework1
+open FsUnit
+open FsCheck
+
+let revTest (list:list<int>) = Functions.listReverse list = List.rev list
+
+/// Test of the function calculating the factorial of n.
+[<Test>]
+let ``List reverse Tests`` () =
+    Check.QuickThrowOnFailure revTest
---
+++ Homework1/Functions/Functions.fs
@@ -0,0 +1,13 @@
+﻿namespace Homework1
+
+/// Module implementing functions for homework.
+module Functions =
+    /// Function that implements list reversal.
+    let rec listReverse list =
+        let rec reverse list reverseList =
+            if List.isEmpty list then
+                reverseList
+            else
+                reverse (List.tail list) (List.head list :: reverseList)
+
+        reverse list []
