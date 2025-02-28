f662fef909e6a7d3682c027586f8920ab6de61d4
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-02-28 18:47:13 +0300

    feat: add tests
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ Homework2/EvenNumbers.Tests/EvenNumbers.Tests.fs
@@ -0,0 +1,31 @@
+﻿module EvenNumbers.Tests
+
+open NUnit.Framework
+open EvenNumbers
+open FsUnit
+open FsCheck
+
+/// Comparing the equivalence of the first and second functions.
+let firstSecondEquivalenceTest (list:list<int>) = 
+    CountEvenNumbers.firstCountEvenNumbers list = CountEvenNumbers.secondCountEvenNumbers list
+
+/// Comparing the equivalence of the second and third functions.
+let SecondThirdEquivalenceTest (list:list<int>) = 
+    CountEvenNumbers.secondCountEvenNumbers list = CountEvenNumbers.thirdCountEvenNumbers list
+
+/// First count even numbers test.
+[<Test>]
+let ``First count even Numbers test`` () =
+    [1; 2; 3] |> CountEvenNumbers.secondCountEvenNumbers |> should equal 1
+    [2; 3; 4; 6] |> CountEvenNumbers.secondCountEvenNumbers |> should equal 3
+    [1; 3; 5] |> CountEvenNumbers.secondCountEvenNumbers |> should equal 0
+    [] |> CountEvenNumbers.secondCountEvenNumbers |> should equal 0
+    [1] |> CountEvenNumbers.secondCountEvenNumbers |> should equal 0
+    [10] |> CountEvenNumbers.secondCountEvenNumbers |> should equal 1
+
+/// Functional Equivalence Test.
+[<Test>]
+let ``Functional equivalence test`` () =
+    Check.QuickThrowOnFailure firstSecondEquivalenceTest
+    Check.QuickThrowOnFailure SecondThirdEquivalenceTest
+
