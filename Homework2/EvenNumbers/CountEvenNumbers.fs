a43c36623a8988a70705ecd18335573a1dace2e2
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-02-28 18:46:56 +0300

    feat: implement three functions for counting even numbers in the list
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ Homework2/EvenNumbers/CountEvenNumbers.fs
@@ -0,0 +1,16 @@
+﻿namespace EvenNumbers
+
+/// Function module for counting the number of even numbers.
+module CountEvenNumbers =
+    /// The function of counting the number of even numbers in the list.
+    let firstCountEvenNumbers list =
+        list |> List.filter (fun x -> x % 2 = 0) |> List.length
+
+    /// The function of counting the number of even numbers in the list.
+    let secondCountEvenNumbers list =
+        list |> List.fold (fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0
+
+    /// The function of counting the number of even numbers in the list.
+    let thirdCountEvenNumbers list =
+        list |> List.map (fun x -> if x % 2 = 0 then 1 else 0) |> List.sum
+    
