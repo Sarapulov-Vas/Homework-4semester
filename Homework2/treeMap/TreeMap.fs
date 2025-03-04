479e544ec7cad4ab7d091257fdebacd64ea5b646
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-03-04 12:42:09 +0300

    feat: implement map for tree
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ Homework2/treeMap/TreeMap.fs
@@ -0,0 +1,14 @@
+﻿namespace TreeMap
+
+module Tree =
+    type Tree<'a> =
+        | Node of 'a * Tree<'a> * Tree<'a>
+        | Empty
+
+    /// Map for binary tree
+    let rec map f binTree =
+        match binTree with
+        | Empty -> Empty
+        | Node(value, left, right) ->
+            Node (f value, map f left, map f right)
+
