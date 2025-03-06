e4c094629f7f67664b65516d00ba1d1b6224d7c8
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-03-06 10:23:28 +0300

    feat: implement the function of calculating the value of the arithmetic expression parse tree
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ Homework2/ParseTree/ParseTree.fs
@@ -0,0 +1,13 @@
+﻿namespace Homework2
+
+module ParseTree =
+    type Tree<'a> =
+        | Operand of 'a
+        | Operator of ('a -> 'a -> 'a) * Tree<'a> * Tree<'a>
+
+    /// Map for binary tree
+    let rec calculate parseTree =
+        match parseTree with
+        | Operand(value) -> value
+        | Operator(operator, left, right) ->
+            operator calculate left calculate right
