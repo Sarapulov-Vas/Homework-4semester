0b72d6d368682fcfad3a52bc70698c89c322595b
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-03-06 10:47:30 +0300

    feat: add tests
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ Homework2/ParseTree.Tests/ParseTree.Tests.fs
@@ -0,0 +1,23 @@
+﻿module ParseTree.Tests
+
+open NUnit.Framework
+open Homework2
+open FsUnit
+
+[<Test>]
+let ```parse tree calculation test`` () =
+    let tree = 
+        ParseTree.Operator((+), ParseTree.Operand 1, ParseTree.Operand 1)
+    ParseTree.calculate tree |> should equal 2
+
+    let tree = 
+        ParseTree.Operator((+), 
+        ParseTree.Operator((/), ParseTree.Operand 10, ParseTree.Operand 2),
+        ParseTree.Operator((*), 
+        ParseTree.Operator((-), ParseTree.Operand 7, ParseTree.Operand 2),
+        ParseTree.Operand 2))
+    ParseTree.calculate tree |> should equal 15
+
+let ```parse tree calculation operand test`` () =
+    let tree =  ParseTree.Operand 1
+    ParseTree.calculate tree |> should equal 1
