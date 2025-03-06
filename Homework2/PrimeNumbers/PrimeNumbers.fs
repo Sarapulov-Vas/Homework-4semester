89f5fc1220b1414a3c0edafeadee1fc1614083d4
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-03-06 22:43:56 +0300

    feat: implement a function generating an infinite sequence of prime numbers
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ Homework2/PrimeNumbers/PrimeNumbers.fs
@@ -0,0 +1,8 @@
+﻿namespace Homework2
+
+module PrimeNumbers =
+    let isPrime n =
+        {2..int(sqrt(float n))} |> Seq.forall (fun x -> n % x <> 0)
+    let primes =
+        Seq.initInfinite (fun x -> x + 2) |> Seq.filter isPrime
+
