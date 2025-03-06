b134bdad7db9392fdb9b4b0976ad36a323634757
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-03-06 22:53:29 +0300

    doc: add comments
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

--- Homework2/PrimeNumbers/PrimeNumbers.fs
+++ Homework2/PrimeNumbers/PrimeNumbers.fs
@@ -1,8 +1,11 @@
 ﻿namespace Homework2
 
 module PrimeNumbers =
+    /// A function to check numbers for a prime number.
     let isPrime n =
         {2..int(sqrt(float n))} |> Seq.forall (fun x -> n % x <> 0)
+    
+    /// Function generating an infinite sequence of prime numbers.
     let primes =
         Seq.initInfinite (fun x -> x + 2) |> Seq.filter isPrime
 
