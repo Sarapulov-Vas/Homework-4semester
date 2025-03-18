103d91c0d784fa5bc90a68a713521301fc40c352
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-03-18 22:39:39 +0300

    feat: add tests
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ Homework4/PhoneBook.Tests/PhoneBook.Tests.fs
@@ -0,0 +1,37 @@
+﻿module PhoneBook.Tests
+
+open NUnit.Framework
+open PhoneBook.Commands
+open FsUnit
+
+[<Test>]
+let ``Add entry to phone book Test`` () =
+    let book = addEntry "Vasilii" "89001231212" []
+    book.Length |> should equal 1
+    book.Head.Name |> should equal "Vasilii"
+    book.Head.Phone |> should equal "89001231212"
+
+[<Test>]
+let ``Find phone by name Test`` () =
+    let book = [{Name = "Vasilii"; Phone = "89001231212"}]
+    findPhoneByName "Vasilii" book|> Option.map (fun entry -> entry.Phone) |>
+        should equal (Some "89001231212")
+
+[<Test>]
+let ``Find name by phone Test`` () =
+    let book = [{Name = "Vasilii"; Phone = "89001231212"}]
+    findNameByPhone "89001231212" book |> Option.map (fun entry -> entry.Name) |>
+        should equal (Some "Vasilii")
+
+[<Test>]
+let ``Load data Test`` () =
+    loadFromFile "../../../TestData/LoadTest.txt" |>
+        should equal (Some [{Name = "Vasilii"; Phone = "89001231212"}])
+
+[<Test>]
+let ``Save and load data Test`` () =
+    let book = [{Name = "Vasilii"; Phone = "89001231212"}]
+    saveToFile "../../../TestData/SaveLoadTest.txt" book
+    loadFromFile "../../../TestData/SaveLoadTest.txt" |>
+        should equal (Some book)
+    
