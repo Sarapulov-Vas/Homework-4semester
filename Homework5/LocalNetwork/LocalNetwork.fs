568bdf8fda1f6bb4992744c6df0de4313a9362ea
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-03-27 23:28:18 +0300

    fix: fix the virus propagation algorithm
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ Homework5/LocalNetwork/LocalNetwork.fs
@@ -0,0 +1,63 @@
+﻿namespace LocalNetwork
+
+open System
+
+type IOperatingSystem =
+    abstract InfectionProbability : float
+
+type OperatingSystem =
+    | Windows
+    | Linux
+    | MacOS
+    interface IOperatingSystem with
+        member this.InfectionProbability =
+            match this with
+            | Windows -> 0.8
+            | Linux -> 0.2
+            | MacOS -> 0.4
+
+type Computer(id: int, os: IOperatingSystem, isInfected: bool) =
+    let mutable isInfected = isInfected
+    
+    member this.Id = id
+    member this.OS = os
+    member this.IsInfected = isInfected
+    
+    member this.TryInfect(random: Random) =
+        if not isInfected && random.NextDouble() < os.InfectionProbability then
+            isInfected <- true
+
+type Network(computers: list<Computer>, net: list<list<bool>>) =
+    let random = Random()
+
+    let isFinished() =
+        { 0 .. computers.Length - 1 }
+        |> Seq.forall (fun i ->
+            { 0 .. computers.Length - 1 }
+            |> Seq.forall (fun j ->
+                not (net[i][j]) || computers[i].IsInfected = computers[j].IsInfected))
+
+    let printState() =
+        for computer in computers do
+            printfn "Computer %d (%A): %s" computer.Id computer.OS (if computer.IsInfected then "Infected" else "Healthy")
+        printfn ""
+
+    let spreadVirus() =
+        let infectList = 
+            [
+                for i = 0 to computers.Length - 1 do
+                    if computers[i].IsInfected then
+                        for j = 0 to computers.Length - 1 do
+                            if net[i][j] && not computers[j].IsInfected then
+                                yield computers[j]
+            ]
+        infectList |> List.iter (fun i -> i.TryInfect(random))
+
+    member this.SpreadVirus = spreadVirus
+    member this.PrintState = printState
+
+    member this.Model =
+        printState()
+        while not (isFinished()) do
+            spreadVirus()
+            printState()
