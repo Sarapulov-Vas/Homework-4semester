167634405373f7a657d1e653c972c10dfd87fb57
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-04-08 18:21:25 +0300

    feat: add tests
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ Homework6/StringCalculations.Tests/StringCalculations.Tests.fs
@@ -0,0 +1,27 @@
+﻿module StringCalculations.Tests
+
+open NUnit.Framework
+open FsUnit
+open StringCalculations.Workflow
+
+[<Test>]
+let ``Valid numbers``() =
+    let result = calculate {
+        let! x = "1"
+        let! y = "2"
+        let! z = "3"
+        return (x + y) * z
+    }
+
+    result |> should equal (Some 9)
+
+[<Test>]
+let ``Invalid number``() =
+    let result = calculate {
+        let! x = "1"
+        let! y = "Ъ"
+        let z = x + y
+        return z
+    }
+
+    result |> should equal None
