bee402409de89ed434f74e638f92492350c7e379
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-06-05 22:17:26 +0300

    feat: implement lazy
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

---
+++ Homework7/Lazy/Lazy.fs
@@ -0,0 +1,46 @@
+﻿namespace Lazy
+
+open System.Threading
+
+type ILazy<'a> =
+    abstract member Get: unit -> 'a
+
+type SingleThreadLazy<'a>(supplier: unit -> 'a) =
+    let mutable result = None
+    interface ILazy<'a> with
+        member this.Get() =
+            match result with
+            | Some value -> value
+            | None ->
+                let value = supplier()
+                result <- Some value
+                value
+
+type MultiThreadLazy<'a>(supplier: unit -> 'a) =
+    let mutable result = None
+    let lockObj = obj()
+    interface ILazy<'a> with
+        member this.Get() =
+            match result with
+            | Some value -> value
+            | None ->
+                lock lockObj (fun () ->
+                    match result with
+                    | Some value -> value
+                    | None ->
+                        let value = supplier()
+                        result <- Some value
+                        value)
+
+type LockFreeLazy<'a>(supplier: unit -> 'a) =
+    let mutable result = None
+    interface ILazy<'a> with
+        member this.Get() =
+            match result with
+            | Some value -> value
+            | None ->
+                let value = supplier()
+                Interlocked.CompareExchange(&result, Some value, None) |> ignore
+                match result with
+                | Some v -> v
+                | None -> value
