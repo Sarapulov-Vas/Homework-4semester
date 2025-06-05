1a3f2c987c94c184b8677c9d0c41b23e5a26a0b9
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2025-06-05 19:41:50 +0300

    doc: add comments
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

--- Homework7/Mini-crawler/Crawler.fs
+++ Homework7/Mini-crawler/Crawler.fs
@@ -5,6 +5,9 @@ open System.Text.RegularExpressions
 
 module Crawler =
     let httpClient = new HttpClient()
+
+    /// Asynchronously loads the contents of a web page at URL
+    /// Returns: page contents
     let downloadPage (url: string) =
         async {
             let! response = httpClient.GetAsync(url) |> Async.AwaitTask
@@ -12,6 +15,8 @@ module Crawler =
             return content
         }
 
+    /// Extracts all absolute HTTP links from HTML page
+    /// Returns: list of found URLs
     let extractLinks page =
         let pattern = "<a\\s+[^>]*href=\"(http[^\"]*)\"[^>]*>"
         let matches = Regex.Matches(page, pattern)
@@ -19,12 +24,16 @@ module Crawler =
             if i.Groups.Count > 1 then
                 yield i.Groups.[1].Value ]
 
+    /// Processes a single page: loads and returns its URL and size
+    /// Returns: tuple (URL, content size)
     let processPage url =
         async {
             let size = (Async.RunSynchronously (downloadPage(url))).Length
             return url, size
         }
 
+    /// Main function: scans the initial page and all links found on it
+    /// Returns: list of tuples (URL, size)
     let crawlPage startUrl =
         async {
             let page = downloadPage startUrl
@@ -36,6 +45,7 @@ module Crawler =
             return results |> Array.toList
         }
 
+    /// Function to output page information
     let printInfo startUrl=
         for (url, size) in Async.RunSynchronously (crawlPage startUrl) do
             printfn "%s - %d" url size
